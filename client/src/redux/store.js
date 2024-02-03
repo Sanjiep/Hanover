0e68c27c7d8af64d55da4421a2aaab4f7e3a9a27
sanjiep.magar <saasmagar554@gmail.com>
2024-02-03 19:05:09 +0545

    redux persist added

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,15 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit';
-import countSlice, { counterSlice } from './reducerSlice/countSlice';
-import boxSlice from './reducerSlice/boxSlice';
-import userSlice from './reducerSlice/userSlice';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import userReducer from './reducerSlice/userSlice'
 import logger from 'redux-logger';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
 
-export default configureStore({
-  reducer: {
-    count: countSlice,
-    box: boxSlice,
-    user: userSlice
-  },
+const reducer = combineReducers({
+  user: userReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, reducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
   middleware: ()=>[logger]
 });
+
+export const persistor = persistStore(store)
